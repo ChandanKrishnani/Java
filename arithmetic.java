d33fadfbb34e910cbfd6e9df4961d33e58927a93
Chandan krishnani <52024480+ChandanKrishnani@users.noreply.github.com>
2020-08-04 13:51:29 +0530

    Create arithmetic.java
    all arithmetic operators in basic java program

---
+++ arithmetic.java
@@ -0,0 +1,16 @@
+class arihtmetic
+{
+    public static void main(String jk[])
+    {
+        int a=34,b=26;
+        int c=a+b;
+        System.out.println(a + "+" +b + "=" +c);
+        c=a-b;
+        System.out.println(a+ "-" +b+ "=" +c);
+        c=a*b;
+        System.out.println(a+ "*" +b+ "=" +c);
+        c=a/b;
+        System.out.println(a+ "/" +b+ "=" +c);
+
+    }
+}
