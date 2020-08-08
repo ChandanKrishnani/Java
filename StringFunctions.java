afc451aa9bb3f05be50d74aa5ab72994c89cd7cc
Chandan krishnani <52024480+ChandanKrishnani@users.noreply.github.com>
2020-08-08 10:40:01 +0530

    Create StringFunctions.java
    Basic string Fucntions

---
+++ StringFunctions.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class StringFunctions
+{
+    public static void main(final String args[])
+    {
+       Scanner s=new Scanner (System.in);
+       System.out.println("Enter the value of a :");
+       char a =s.next().charAt(0);
+       System.out.println(a);    
+    }
+
+}
