c8813f1e7e448fab5c02a0a29efc084ed6dca5e6
Chandan krishnani <52024480+ChandanKrishnani@users.noreply.github.com>
2020-08-08 10:17:02 +0530

    Create Runtimeinput.java
    taking input in runtime using scanner class and multiplying them

---
+++ Runtimeinput.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Runtimeinput
+{
+    public static void main(final String args[])
+    {
+       Scanner s=new Scanner (System.in);
+       System.out.println("Enter the value of a :");
+       int a =s.nextInt();
+       System.out.println("Enter the value of a :");
+       int b =s.nextInt();
+       int c=a*b;
+       System.out.println(c);    
+    }
+
+}
