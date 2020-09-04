c381217d2eedc215c67e36b34beb01e43e242d6e
Chandan krishnani <52024480+ChandanKrishnani@users.noreply.github.com>
2020-09-04 15:11:54 +0530

    Create Pattern.java
     Write a program to print the pattern: 
    1 
    1 2
    1 2 3 
    1 2 3 4 
    1 2 3 4 5

---
+++ Pattern.java
@@ -0,0 +1,18 @@
+class hello
+{
+ 	   public static void main(final String args[])
+	{
+		    for(int i=1;i<=5;i++)
+		    {
+                                   //Nested loop one for next line other one for numbers 
+            		for(int j=1;j<=i;j++)
+           			 { 
+                                              //to print number 
+
+                			System.out.print(j + " ");
+            		}
+                        //to take the cursor to the next line 
+            	System.out.println();
+	        	}
+	}
+}
